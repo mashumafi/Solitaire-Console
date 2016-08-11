a346ff5013c08643ced4de38fccb27845e1047a6
Matthew Murphy <nonplayablecharacter@hotmail.com>
2016-08-11 12:33:32 +0000

    updating readme files

--- fp/src/fp.cpp
+++ fp/src/fp.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <iostream>
 
+#include <boost/phoenix/stl/algorithm.hpp>
+
 fp::~fp()
 {
 }
@@ -18,14 +20,25 @@ int fp::main()
   auto add = arg1 + arg2;
   auto print = std::cout << arg1 << std::endl;
   auto isodd = if_(arg1 > 2 && arg1 % 2 == 1)
-    [
-      bind(arg2, arg1)
-    ];
+  [
+    bind(arg2, arg1),
+    bind(arg2, arg1)
+  ]
+  .else_
+  [
+    bind(arg2, "err")
+  ];
   isodd(add(1, 2), print);
-  std::cout << _1 << std::endl;
+  isodd(add(1, 3), print);
 
   boost::phoenix::function<is_odd_impl> is_odd;
-  std::cout << std::count_if(v.begin(), v.end(), is_odd(arg1)) << '\n';
+  std::cout << std::count_if(v.begin(), v.end(), is_odd(arg1)) << std::endl;
+  std::cout << std::endl;
+  
+  int array[] = {1, 2, 3};
+  int output[3];
+  copy(arg1, arg2)(array, output);
+  std::for_each(output, output+3, print);
 
   return 0;
 }
