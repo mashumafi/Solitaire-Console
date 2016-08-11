0abda1f169839a8ec855aa9b61ef83f2e9a82f68
Matthew Murphy <nonplayablecharacter@hotmail.com>
2016-08-11 17:44:25 +0000

    irc client project added

---
+++ irc/include/IRCClient.hpp
@@ -0,0 +1,400 @@
+// Based on https://tools.ietf.org/html/rfc2812
+
+#pragma once
+
+#include <Main.hpp>
+
+#include <cstring>
+#include <vector>
+#include <iostream>
+
+#include <boost/asio.hpp>
+#include <boost/regex.hpp>
+#include <boost/algorithm/string/join.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
+
+using boost::asio::ip::tcp;
+
+enum Reply
+{
+  RPL_WELCOME           = 1,
+  RPL_YOURHOST          = 2,
+  RPL_CREATED           = 3,
+  RPL_MYINFO            = 4,
+  RPL_BOUNCE            = 5,
+  RPL_TRACELINK         = 200,
+  RPL_TRACECONNECTING   = 201,
+  RPL_TRACEHANDSHAKE    = 202,
+  RPL_TRACEUNKNOWN      = 203,
+  RPL_TRACEOPERATOR     = 204,
+  RPL_TRACEUSER         = 205,
+  RPL_TRACESERVER       = 206,
+  RPL_TRACESERVICE      = 207,
+  RPL_TRACENEWTYPE      = 208,
+  RPL_TRACECLASS        = 209,
+  RPL_TRACERECONNECT    = 210,
+  RPL_STATSLINKINFO     = 211,
+  RPL_STATSCOMMANDS     = 212,
+  RPL_STATSCLINE        = 213,
+  RPL_STATSNLINE        = 214,
+  RPL_STATSILINE        = 215,
+  RPL_STATSKLINE        = 216,
+  RPL_STATSQLINE        = 217,
+  RPL_STATSYLINE        = 218,
+  RPL_ENDOFSTATS        = 219,
+  RPL_UMODEIS           = 221,
+  RPL_SERVICEINFO       = 231,
+  RPL_ENDOFSERVICES     = 232,
+  RPL_SERVICE           = 233,
+  RPL_SERVLIST          = 234,
+  RPL_SERVLISTEND       = 235,
+  RPL_STATSVLINE        = 240,
+  RPL_STATSLLINE        = 241,
+  RPL_STATSUPTIME       = 242,
+  RPL_STATSOLINE        = 243,
+  RPL_STATSHLINE        = 244,
+  RPL_STATSSLINE        = 244,
+  RPL_STATSPING         = 246,
+  RPL_STATSBLINE        = 247,
+  RPL_STATSDLINE        = 250,
+  RPL_LUSERCLIENT       = 251,
+  RPL_LUSEROP           = 252,
+  RPL_LUSERUNKNOWN      = 253,
+  RPL_LUSERCHANNELS     = 254,
+  RPL_LUSERME           = 255,
+  RPL_ADMINME           = 256,
+  RPL_ADMINLOC1         = 257,
+  RPL_ADMINLOC2         = 258,
+  RPL_ADMINEMAIL        = 259,
+  RPL_TRACELOG          = 261,
+  RPL_TRACEEND          = 262,
+  RPL_TRYAGAIN          = 263,
+  RPL_NONE              = 300,
+  RPL_AWAY              = 301,
+  RPL_USERHOST          = 302,
+  RPL_ISON              = 303,
+  RPL_UNAWAY            = 305,
+  RPL_NOWAWAY           = 306,
+  RPL_WHOISUSER         = 311,
+  RPL_WHOISSERVER       = 312,
+  RPL_WHOISOPERATOR     = 313,
+  RPL_WHOWASUSER        = 314,
+  RPL_ENDOFWHO          = 315,
+  RPL_WHOISCHANOP       = 316,
+  RPL_WHOISIDLE         = 317,
+  RPL_ENDOFWHOIS        = 318,
+  RPL_WHOISCHANNELS     = 319,
+  RPL_LISTSTART         = 321,
+  RPL_LIST              = 322,
+  RPL_LISTEND           = 323,
+  RPL_CHANNELMODEIS     = 324,
+  RPL_UNIQOPIS          = 325,
+  RPL_NOTOPIC           = 331,
+  RPL_TOPIC             = 332,
+  RPL_INVITING          = 341,
+  RPL_SUMMONING         = 342,
+  RPL_INVITELIST        = 346,
+  RPL_ENDOFINVITELIST   = 347,
+  RPL_EXCEPTLIST        = 348,
+  RPL_ENDOFEXCEPTLIST   = 349,
+  RPL_VERSION           = 351,
+  RPL_WHOREPLY          = 352,
+  RPL_NAMREPLY          = 353,
+  RPL_KILLDONE          = 361,
+  RPL_CLOSING           = 362,
+  RPL_CLOSEEND          = 363,
+  RPL_LINKS             = 364,
+  RPL_ENDOFLINKS        = 365,
+  RPL_ENDOFNAMES        = 366,
+  RPL_BANLIST           = 367,
+  RPL_ENDOFBANLIST      = 368,
+  RPL_ENDOFWHOWAS       = 369,
+  RPL_INFO              = 371,
+  RPL_MOTD              = 372,
+  RPL_INFOSTART         = 373,
+  RPL_ENDOFINFO         = 374,
+  RPL_MOTDSTART         = 375,
+  RPL_ENDOFMOTD         = 376,
+  RPL_YOUREOPER         = 381,
+  RPL_REHASHING         = 382,
+  RPL_YOURESERVICE      = 383,
+  RPL_MYPORTIS          = 384,
+  RPL_TIME              = 391,
+  RPL_USERSSTART        = 392,
+  RPL_USERS             = 393,
+  RPL_ENDOFUSERS        = 394,
+  RPL_NOUSERS           = 395,
+  ERR_NOSUCHNICK        = 401,
+  ERR_NOSUCHSERVER      = 402,
+  ERR_NOSUCHCHANNEL     = 403,
+  ERR_CANNOTSENDTOCHAN  = 404,
+  ERR_TOOMANYCHANNELS   = 405,
+  ERR_WASNOSUCHNICK     = 406,
+  ERR_TOOMANYTARGETS    = 407,
+  ERR_NOSUCHSERVICE     = 408,
+  ERR_NOORIGIN          = 409,
+  ERR_NORECIPIENT       = 411,
+  ERR_NOTEXTTOSEND      = 412,
+  ERR_NOTOPLEVEL        = 413,
+  ERR_WILDTOPLEVEL      = 414,
+  ERR_BADMASK           = 415,
+  ERR_UNKNOWNCOMMAND    = 421,
+  ERR_NOMOTD            = 422,
+  ERR_NOADMININFO       = 423,
+  ERR_FILEERROR         = 424,
+  ERR_NONICKNAMEGIVEN   = 431,
+  ERR_ERRONEUSNICKNAME  = 432,
+  ERR_NICKNAMEINUSE     = 433,
+  ERR_NICKCOLLISION     = 436,
+  ERR_UNAVAILRESOURCE   = 437,
+  ERR_USERNOTINCHANNEL  = 441,
+  ERR_NOTONCHANNEL      = 442,
+  ERR_USERONCHANNEL     = 443,
+  ERR_NOLOGIN           = 444,
+  ERR_SUMMONDISABLED    = 445,
+  ERR_USERSDISABLED     = 446,
+  ERR_NOTREGISTERED     = 451,
+  ERR_NEEDMOREPARAMS    = 461,
+  ERR_ALREADYREGISTRED  = 462,
+  ERR_NOPERMFORHOST     = 463,
+  ERR_PASSWDMISMATCH    = 464,
+  ERR_YOUREBANNEDCREEP  = 465,
+  ERR_YOUWILLBEBANNED   = 466,
+  ERR_KEYSET            = 467,
+  ERR_CHANNELISFULL     = 471,
+  ERR_UNKNOWNMODE       = 472,
+  ERR_INVITEONLYCHAN    = 473,
+  ERR_BANNEDFROMCHAN    = 474,
+  ERR_BADCHANNELKEY     = 475,
+  ERR_BADCHANMASK       = 476,
+  ERR_NOCHANMODES       = 477,
+  ERR_BANLISTFULL       = 478,
+  ERR_NOPRIVILEGES      = 481,
+  ERR_CHANOPRIVSNEEDED  = 482,
+  ERR_CANTKILLSERVER    = 483,
+  ERR_RESTRICTED        = 484,
+  ERR_UNIQOPPRIVSNEEDED = 485,
+  ERR_NOOPERHOST        = 491,
+  ERR_NOSERVICEHOST     = 492,
+  ERR_UMODEUNKNOWNFLAG  = 501,
+  ERR_USERSDONTMATCH    = 502
+};
+
+// https://gist.github.com/datagrok/380449c30fd0c5cf2f30
+
+class Message
+{
+public:
+  Message(std::string raw)
+  {
+    const std::string regex_str = "^"                 // We'll match the whole line. Start.
+                                  "(?:@(.*?)\\ )?"    // Match Tags
+                                                      // Optional prefix and the space that separates it
+                                                      // from the next thing. Prefix can be a servername,
+                                                      // or nick[[!user]@host]
+                                  "(?::("             // This whole set is optional but if it's
+                                                      // here it begins with : and ends with space
+                                  "([^@!\\ ]*)"       // nick
+                                  "(?:"               // then, optionally user/host
+                                  "(?:"               // but user is optional if host is given
+                                  "!([^@]*)"          // !user
+                                  ")?"                // (user was optional)
+                                  "@([^\\ ]*)"        // @host
+                                  ")?"                // (host was optional)
+                                  ")\\ )?"            // ":nick!user@host " ends
+                                  "([^\\ ]+)"         // IRCClient command (required)
+                                                      // Optional args, max 15, space separated. Last arg is
+                                                      // the only one that may contain inner spaces. More than
+                                                      // 15 words means remainder of words are part of 15th arg.
+                                                      // Last arg may be indicated by a colon prefix instead.
+                                                      // Pull the leading and last args out separately; we have
+                                                      // to split the former on spaces.
+                                  "("
+                                  "(?:"
+                                  "\\ [^:\\ ][^\\ ]*" // space, no colon, non-space characters
+                                  "){0,14}"           // repeated up to 14 times
+                                  ")"                 // captured in one reference
+                                  "(?:\\ :?(.*))?"    // the rest, does not capture colon.
+                                  "$";                // EOL
+    boost::regex expr{regex_str};
+    boost::smatch what;
+    if (boost::regex_search(raw, what, expr))
+    {
+      this->m_raw = raw;
+      std::string tags_str(what[1]);
+      std::vector<std::string> tags;
+      boost::split(tags, tags_str, boost::is_any_of(";"));
+      for(unsigned int i = 0; i < tags.size(); i++)
+      {
+        std::vector<std::string> tag;
+        boost::split(tag, tags[i], boost::is_any_of("="));
+        if(tag[0].length() > 0)
+        {
+          if(tag.size() == 2)
+          {
+            m_tags.insert(std::pair<std::string, std::string>(tag[0], tag[1]));
+          }
+          else if(tag.size() == 1)
+          {
+            m_tags.insert(std::pair<std::string, std::string>(tag[0], ""));
+          }
+          else
+          {
+            std::cout << tags[i] << std::endl;
+          }
+        }
+      }
+      this->prefix = what[2];
+      this->nickname = what[3];
+      this->user = what[4];
+      this->host = what[5];
+      this->command = what[6];
+      std::string params = std::string(what[7]);
+      boost::split(this->params, params, boost::is_any_of(" "));
+      this->params.erase(this->params.begin());
+      if(std::string(what[8]).length() > 0); this->params.push_back(what[8]);
+      //std::cout << msg.prefix << " " << msg.nickname << " " << msg.user << " " << msg.command << " " << msg.params.size() << std::endl;
+    }
+  }
+  
+  std::string toString() const
+  {
+    std::string tags = "    \"tags\": [\r\n";
+    for(auto elem : m_tags)
+    {
+      tags += str(boost::format("        \"%1%\": \"%2%\",\r\n") % elem.first % elem.second);
+    }
+    tags += "    ]\r\n";
+    return str(boost::format("{\r\n    \"nickname\": \"%1%\",\r\n    \"user\": \"%2%\",\r\n    \"host\": \"%3%\",\r\n    \"command\": \"%4%\",\r\n    \"params\": \"%5%\",\r\n%6%}") % nickname % user % host % command % boost::algorithm::join(params, ":") % tags);
+  }
+  const std::string& raw() const
+  {
+    return m_raw;
+  }
+  const std::map<std::string, std::string>& tags() const
+  {
+    return m_tags;
+  }
+private:
+  std::string m_raw;
+  std::map<std::string, std::string> m_tags;
+public:
+  std::string prefix;
+  std::string nickname;
+  std::string user;
+  std::string host;
+  std::string command;
+  std::vector<std::string> params;
+};
+
+class IRCClient : Main
+{
+public:
+  IRCClient(const std::string& host, const std::string& port, const std::string& nick, const std::string& pass);
+  virtual ~IRCClient();
+  
+  void close();
+  
+  virtual void send(const std::string& command, const std::vector<std::string>& params);
+  virtual void send(const std::string& command);
+  
+  virtual void pass(const std::string& password);
+  
+  virtual void nick(const std::string& nickname);
+  
+  virtual void user(const std::string& user, const unsigned int& mode, const std::string& unused = "*", const std::string& realname = "");
+  
+  virtual void oper(const std::string& name, const std::string& password);
+  
+  virtual void mode(const std::string& nickname, const std::string& mode);
+  
+  virtual void service(const std::string& nickname, const std::string& reserved1, const std::string& distribution, const std::string& type, const std::string& reserved2, const std::string& info);
+  
+  virtual void quit(const std::string& quit_message = "");
+  
+  virtual void squit(const std::string& server, const std::string& comment);
+  
+  virtual void join0();
+  virtual void join(const std::vector<std::string>& channels, const std::vector<std::string>& keys = std::vector<std::string>());
+  virtual void join(const std::string& channels, const std::string& keys = "");
+  
+  virtual void part(const std::vector<std::string>& channels, const std::string& part_message = "");
+  virtual void part(const std::string& channels, const std::string& part_message = "");
+  
+  virtual void mode(const std::string& channel, const std::string& modes, const std::string& modeparams);
+  
+  virtual void topic(const std::string& channel, const std::string& topic = "");
+  
+  virtual void names(const std::vector<std::string>& channels, const std::string& target = "");
+  virtual void names(const std::string& channels, const std::string& target = "");
+  
+  virtual void list(const std::vector<std::string>& channels, const std::string& target = "");
+  virtual void list(const std::string& channels, const std::string& target = "");
+  
+  virtual void invite(const std::string& nickname, const std::string& channel);
+  
+  virtual void kick(const std::vector<std::string>& channels, const std::vector<std::string>& users, const std::string& comment = "");
+  
+  virtual void privmsg(const std::string& msgtarget, const std::string& text_to_be_sent);
+  
+  virtual void notice(const std::string& msgtarget, const std::string& text);
+  
+  virtual void motd(const std::string& target = "");
+  
+  virtual void lusers(const std::string& mask = "", const std::string& target = "");
+  
+  virtual void version(const std::string& target = "");
+  
+  virtual void stats(const std::string& query = "", const std::string& target = "");
+  
+  virtual void links(const std::string& remote_server = "", const std::string& server_mask = "");
+  
+  virtual void time(const std::string& target = "");
+  
+  virtual void connect(const std::string& target_server, const std::string& port, const std::string& remote_server = "");
+  
+  virtual void trace(const std::string& target = "");
+  
+  virtual void admin(const std::string& target = "");
+  
+  virtual void info(const std::string& target = "");
+  
+  virtual void servlist(const std::string& mask = "", const std::string& type = "");
+  
+  virtual void squery(const std::string& servicename, const std::string& text);
+  
+  virtual void who(const std::string& mask = "", const std::string& parameter = "");
+  
+  virtual void whois(const std::string& mask = "", const std::string& target = "");
+  
+  virtual void whowas(const std::string& nicknames, unsigned int count = -1, const std::string& target = "");
+  
+  virtual void kill(const std::string& nickname, const std::string& comment);
+
+  virtual void ping(const std::string& server, const std::string& server2 = "");
+  
+  virtual void pong(const std::string& server, const std::string& server2 = "");
+  
+  virtual void error(const std::string& error_message);
+protected:
+  virtual bool reply(const Message&, const Reply code);
+  virtual bool message(const Message&);
+  
+  virtual void join(const Message&, const std::vector<std::string>& channel, const std::vector<std::string>& keys);
+  virtual void ping(const Message& msg, const std::string& server, const std::string& server2 = "");
+  virtual void privmsg(const Message&, const std::string&, const std::string&);
+  virtual void part(const Message&);
+  virtual void connected(void);
+  virtual void read(const std::string& msg);
+  virtual void read(const Message& msg);
+  
+private:
+  void connect(tcp::resolver::iterator endpoint_iterator);
+  void read();
+  tcp::socket* s;
+  boost::asio::io_service io_service;
+  boost::asio::streambuf response;
+  std::string m_nick;
+  std::string m_pass;
+};
---
+++ irc/src/IRCClient.cpp
@@ -0,0 +1,374 @@
+#include <IRCClient.hpp>
+
+#include <stdarg.h>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include <boost/thread.hpp>
+#include <boost/assign/list_of.hpp>
+#include <boost/algorithm/string.hpp>
+
+using namespace boost::assign;
+
+IRCClient::IRCClient(const std::string& host, const std::string& port, const std::string& nick, const std::string& pass) : m_nick(nick), m_pass(pass)
+{
+  tcp::resolver resolver(io_service);
+  tcp::resolver::query query(tcp::v4(), host, port);
+  tcp::resolver::iterator iterator = resolver.resolve(query);
+
+  s = new tcp::socket(io_service);
+  connect(iterator);
+  boost::thread t([this](){ io_service.run(); });
+}
+
+IRCClient::~IRCClient()
+{
+  io_service.stop();
+  s->close();
+  delete s;
+}
+
+void IRCClient::send(const std::string& cmd, const std::vector<std::string>& vec)
+{
+  send(str(boost::format("%1% %2%") % cmd % boost::algorithm::join(vec, " ")));
+}
+
+void IRCClient::send(const std::string& raw)
+{
+  std::string* msg = new std::string(raw + "\r\n");
+  boost::asio::async_write(*s, boost::asio::buffer(msg->c_str(), msg->length()), [msg] (boost::system::error_code ec, std::size_t)
+  {
+    std::cout << "< " << *msg;
+    delete msg;
+  });
+}
+
+bool IRCClient::message(const Message& msg)
+{
+  switch(msg.command[0])
+  {
+    case 'P':
+    {
+      switch(msg.command[1])
+      {
+        case 'R': // PRIVMSG
+        {
+          privmsg(msg, msg.params[0], msg.params[1]);
+          return false;
+        }
+        case 'I': // PING
+        {
+          if(msg.params.size() == 1)
+          {
+            ping(msg, msg.params[0]);
+          }
+          else if(msg.params.size() > 1)
+          {
+            ping(msg, msg.params[0], msg.params[1]);
+          }
+          return false;
+        }
+      }
+      return true;
+    }
+    case 'J': // JOIN
+    {
+      std::vector<std::string> channels;
+      std::vector<std::string> keys;
+      if(msg.params.size() > 0)
+      {
+        boost::split(channels, msg.params[0], boost::is_any_of(","));
+      }
+      if(msg.params.size() > 1)
+      {
+        boost::split(keys, msg.params[1], boost::is_any_of(","));
+      }
+      join(msg, channels, keys);
+      return false;
+    }
+  }
+  return true;
+}
+
+bool IRCClient::reply(const Message& msg, const Reply code)
+{
+  return true;
+}
+
+void IRCClient::pass(const std::string& password)
+{
+  send("PASS", list_of(password));
+}
+
+void IRCClient::nick(const std::string& nickname)
+{
+  send("NICK", list_of(nickname));
+}
+  
+void IRCClient::user(const std::string& user, const unsigned int& mode, const std::string& unused, const std::string& realname)
+{
+}
+
+void IRCClient::oper(const std::string& name, const std::string& password)
+{
+}
+
+void IRCClient::mode(const std::string& nickname, const std::string& mode)
+{
+}
+
+void IRCClient::service(const std::string& nickname, const std::string& reserved1, const std::string& distribution, const std::string& type, const std::string& reserved2, const std::string& info)
+{
+}
+
+void IRCClient::quit(const std::string& quit_message)
+{
+  send("QUIT", list_of(quit_message));
+}
+
+void IRCClient::squit(const std::string& server, const std::string& comment)
+{
+}
+
+void IRCClient::join(const std::vector<std::string>& channels, const std::vector<std::string>& keys)
+{
+  join(boost::algorithm::join(channels, ","), boost::algorithm::join(keys, ","));
+}
+
+void IRCClient::join(const std::string& channels, const std::string& keys)
+{
+  send("JOIN", list_of(channels)(keys));
+}
+
+void IRCClient::join0()
+{
+  send("JOIN", list_of("0"));
+}
+
+void IRCClient::join(const Message& msg, const std::vector<std::string>& channels, const std::vector<std::string>& keys)
+{
+  std::cout << boost::format("%1% joined %2%") % msg.nickname % boost::algorithm::join(channels, ",") << std::endl;
+}
+
+void IRCClient::part(const std::vector<std::string>& channels, const std::string& part_message)
+{
+  part(boost::algorithm::join(channels, ","), part_message);
+}
+
+void IRCClient::part(const std::string& channels, const std::string& part_message)
+{
+  send("PART", list_of(channels)(part_message));
+}
+
+void IRCClient::part(const Message& msg)
+{
+}
+
+void IRCClient::mode(const std::string& channel, const std::string& modes, const std::string& modeparams)
+{
+}
+
+void IRCClient::topic(const std::string& channel, const std::string& topic)
+{
+}
+
+void IRCClient::names(const std::vector<std::string>& channels, const std::string& target)
+{
+}
+
+void IRCClient::names(const std::string& channels, const std::string& target)
+{
+}
+
+void IRCClient::list(const std::vector<std::string>& channels, const std::string& target)
+{
+}
+
+void IRCClient::list(const std::string& channels, const std::string& target)
+{
+}
+
+void IRCClient::invite(const std::string& nickname, const std::string& channel)
+{
+  
+}
+
+void IRCClient::kick(const std::vector<std::string>& channels, const std::vector<std::string>& users, const std::string& comment)
+{
+  
+}
+
+void IRCClient::privmsg(const std::string& msgtarget, const std::string& text_to_be_sent)
+{
+  send("PRIVMSG", list_of(msgtarget)(":" + text_to_be_sent));
+}
+
+void IRCClient::privmsg(const Message& msg, const std::string& msgtarget, const std::string& text_to_be_sent)
+{
+  std::cout << msgtarget << "> " << msg.nickname << ": " << text_to_be_sent << std::endl;
+}
+
+void IRCClient::notice(const std::string& msgtarget, const std::string& text)
+{
+}
+
+void IRCClient::motd(const std::string& target)
+{
+}
+
+void IRCClient::lusers(const std::string& mask, const std::string& target)
+{
+}
+
+void IRCClient::version(const std::string& target)
+{
+}
+
+void IRCClient::stats(const std::string& query, const std::string& target)
+{
+}
+
+void IRCClient::links(const std::string& remote_server, const std::string& server_mask)
+{
+}
+
+void IRCClient::time(const std::string& target)
+{
+}
+
+void IRCClient::connect(const std::string& target_server, const std::string& port, const std::string& remote_server)
+{
+}
+
+void IRCClient::trace(const std::string& target)
+{
+}
+
+void IRCClient::admin(const std::string& target)
+{
+}
+
+void IRCClient::info(const std::string& target)
+{
+}
+  
+void IRCClient::servlist(const std::string& mask, const std::string& type)
+{
+}
+
+void IRCClient::squery(const std::string& servicename, const std::string& text)
+{
+}
+
+void IRCClient::who(const std::string& mask, const std::string& parameter)
+{
+}
+
+void IRCClient::whois(const std::string& mask, const std::string& target)
+{
+}
+
+void IRCClient::whowas(const std::string& nicknames, unsigned int count, const std::string& target)
+{
+}
+
+void IRCClient::kill(const std::string& nickname, const std::string& comment)
+{
+  
+}
+
+void IRCClient::ping(const std::string& server, const std::string& server2)
+{
+  send("PING", list_of(server)(server2));
+}
+
+void IRCClient::ping(const Message& msg, const std::string& server, const std::string& server2)
+{
+  pong(server, server2);
+}
+
+void IRCClient::pong(const std::string& server, const std::string& server2)
+{
+  send("PONG", list_of(server)(server2));
+}
+
+void IRCClient::error(const std::string& error_message)
+{
+  
+}
+
+void IRCClient::connect(tcp::resolver::iterator endpoint_iterator)
+{
+  boost::asio::async_connect(*s, endpoint_iterator,
+    [this](boost::system::error_code ec, tcp::resolver::iterator)
+    {
+      if (!ec)
+      {
+        read();
+        connected();
+      }
+      else
+      {
+      }
+    });
+}
+
+void IRCClient::read()
+{
+  boost::asio::async_read_until(*s, response, "\r\n",
+    [this](boost::system::error_code ec, std::size_t)
+    {
+      if (!ec)
+      {
+        std::istream response_stream(&response);
+        std::string line;
+        do
+        {
+          getline(response_stream, line);
+          read(line);
+        } while(response.size() > 0);
+        read();
+      }
+      else
+      {
+        s->close();
+      }
+    });
+}
+
+void IRCClient::read(const std::string& msg)
+{
+  read(Message(msg));
+}
+
+void IRCClient::read(const Message& msg)
+{
+  try
+  {
+    if(reply(msg, static_cast<Reply>(stoi(msg.command))))
+    {
+      std::cout << "> " << msg.raw() << std::endl;
+    }
+  }
+  catch(std::invalid_argument)
+  {
+    if(message(msg))
+    {
+      std::cout << "> " << msg.raw() << std::endl;
+    }
+  }
+}
+
+void IRCClient::connected()
+{
+  pass(m_pass);
+  nick(m_nick);
+}
+
+void IRCClient::close()
+{
+  io_service.post([this]() { s->close(); });
+}
