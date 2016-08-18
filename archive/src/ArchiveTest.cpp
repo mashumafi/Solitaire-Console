6cf147c1ef82ed009c90b610582fcdc7a6a35ec2
Matthew Murphy <nonplayablecharacter@hotmail.com>
2016-08-18 23:15:32 +0000

    new archive tests

--- archive/src/ArchiveTest.cpp
+++ archive/src/ArchiveTest.cpp
@@ -1,6 +1,6 @@
 #include <Archive.hpp>
 
-#if !defined( WIN32 )
+#ifndef WIN32
 #define BOOST_TEST_DYN_LINK
 #endif
 #define BOOST_TEST_MODULE archive_test_module
@@ -14,7 +14,6 @@ BOOST_AUTO_TEST_CASE(get_root_out)
   Archive oarchive("test.db");
   DirectoryStream* root = oarchive.getRoot();
   BOOST_CHECK(root->name() == "root");
-  BOOST_CHECK(root->make("child")->name() == "child");
   oarchive.close();
 }
 
@@ -23,9 +22,26 @@ BOOST_AUTO_TEST_CASE(get_root_in)
   Archive iarchive("test.db");
   DirectoryStream* root = iarchive.getRoot();
   BOOST_CHECK(root->name() == "root");
-  DirectoryStream* child = root->getAbsDir(0);
-  BOOST_CHECK(child->name() == "child");
   iarchive.close();
 }
+
+BOOST_AUTO_TEST_CASE(add_child_out)
+{
+  std::remove("test.db");
+  Archive oarchive("test.db");
+  DirectoryStream* root = oarchive.getRoot();
+  BOOST_CHECK(root->make("child")->name() == "child");
+  oarchive.close();
+}
+
+BOOST_AUTO_TEST_CASE(add_child_in)
+{
+  Archive iarchive("test.db");
+  DirectoryStream* root = iarchive.getRoot();
+  BOOST_CHECK(root->getAbsDir(0)->name() == "child");
+  iarchive.close();
+}
+
+
 
 BOOST_AUTO_TEST_SUITE_END()
